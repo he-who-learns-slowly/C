68fee8a9013d6c83fd75a4eec188d64d8bbedd1b
he-who-learns-slowly <58092581+he-who-learns-slowly@users.noreply.github.com>
2021-03-24 21:44:38 -0700

    updates to array

--- array.c
+++ array.c
@@ -1,5 +1,18 @@
-#include<stdio.c>
-
+#include<stdio.h>
+#include<stdlib.h>
 int main(int argc, char** argv){
-	return 0
+	
+	int* MyArray;
+	int size = 10;
+	MyArray = (int*)malloc(size*sizeof(int));
+	int i;
+	for (i = 0; i <10; i++){
+		MyArray[i]= i;
+	}
+	i = 0;
+	for (i = 0; i <10; i++){
+		printf("%i, ", MyArray[i]);
+	}
+	free(MyArray);
+	return 0;
 }
